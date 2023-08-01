33d36e9258d39fd8e420bf64f0653ae773e02d1f
abraralvinirab <abrar15-9800@diu.edu.bd>
2023-08-02 00:49:53 +0600

    add mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,57 @@
-import  mongoose from 'mongoose';
-import { app } from './app';
-const port:number = Number(process.env.PORT) || 5000
+// import  mongoose from 'mongoose';
+// import { app } from './app';
+// const port:number = Number(process.env.PORT) || 5000
 
+// const dataBaseUrl: string = process.env.DATABASE_URL || "default_connection_string";
+
+// console.log(dataBaseUrl)
+
+// //db connection
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//     try{
+//           await mongoose.connect(dataBaseUrl);
+//   console.log('db coonect succcesfully');
+//     }
+//     catch(err){
+//         console.log('error occured in coonection',err);
+//     }
+
+// }
+// main();
+
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`)
+// })
+
+
+///mongodb
+
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const port: number = Number(process.env.PORT) || 5000;
 const dataBaseUrl: string = process.env.DATABASE_URL || "default_connection_string";
 
-console.log(dataBaseUrl)
-
-//db connection
-
-main().catch(err => console.log(err));
+console.log(dataBaseUrl);
 
 async function main() {
-    try{
-          await mongoose.connect(dataBaseUrl);
-  console.log('db coonect succcesfully');
-    }
-    catch(err){
-        console.log('error occured in coonection',err);
-    }
-
+  try {
+    await mongoose.connect(dataBaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Add any other options specific to your MongoDB configuration
+    });
+    console.log('db connect successfully');
+  } catch (err) {
+    console.log('error occurred in connection', err);
+  }
 }
-main();
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+main().then(() => {
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+});
