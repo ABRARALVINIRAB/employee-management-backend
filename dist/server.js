54bfe37d454a29f2f3c811eccb6b5a692b7b6717
abraralvinirab <abrar15-9800@diu.edu.bd>
2023-08-02 03:08:40 +0600

    deploy in vercel

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,4 @@
 "use strict";
-// import  mongoose from 'mongoose';
-// import { app } from './app';
-// const port:number = Number(process.env.PORT) || 5000
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,46 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const dataBaseUrl: string = process.env.DATABASE_URL || "default_connection_string";
-// console.log(dataBaseUrl)
-// //db connection
-// main().catch(err => console.log(err));
-// async function main() {
-//     try{
-//           await mongoose.connect(dataBaseUrl);
-//   console.log('db coonect succcesfully');
-//     }
-//     catch(err){
-//         console.log('error occured in coonection',err);
-//     }
-// }
-// main();
-// app.listen(port, () => {
-//   console.log(`Server is listening on port ${port}`)
-// })
-///mongodb
-// import mongoose from 'mongoose';
-// import { app } from './app';
-// const port: number = Number(process.env.PORT) || 5000;
-// const dataBaseUrl: string = process.env.DATABASE_URL || "default_connection_string";
-// console.log(dataBaseUrl);
-// async function main() {
-//   try {
-//     await mongoose.connect(dataBaseUrl, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       // Add any other options specific to your MongoDB configuration
-//     });
-//     console.log('db connect successfully');
-//   } catch (err) {
-//     console.log('error occurred in connection', err);
-//   }
-// }
-// main().then(() => {
-//   app.listen(port, () => {
-//     console.log(`Server is listening on port ${port}`);
-//   });
-// });
 const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = require("./app");
 const port = Number(process.env.PORT) || 5000;
@@ -62,17 +19,12 @@ const dataBaseUrl = process.env.DATABASE_URL || "default_connection_string";
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            // Use custom connection options type
-            const options = {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-                // Add any other options specific to your MongoDB configuration
-            };
-            yield mongoose_1.default.connect(dataBaseUrl, options);
-            console.log('db connect successfully');
+            yield mongoose_1.default.connect(dataBaseUrl);
+            console.log('🛢 database connected succcesfully');
+            //listen
         }
         catch (err) {
-            console.log('error occurred in connection', err);
+            console.log('failed to coonet in database', err);
         }
     });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,96 +1,20 @@
-// import  mongoose from 'mongoose';
-// import { app } from './app';
-// const port:number = Number(process.env.PORT) || 5000
-
-// const dataBaseUrl: string = process.env.DATABASE_URL || "default_connection_string";
-
-// console.log(dataBaseUrl)
-
-// //db connection
-
-// main().catch(err => console.log(err));
-
-// async function main() {
-//     try{
-//           await mongoose.connect(dataBaseUrl);
-//   console.log('db coonect succcesfully');
-//     }
-//     catch(err){
-//         console.log('error occured in coonection',err);
-//     }
-
-// }
-// main();
-
-// app.listen(port, () => {
-//   console.log(`Server is listening on port ${port}`)
-// })
-
-
-///mongodb
-
-// import mongoose from 'mongoose';
-// import { app } from './app';
-
-// const port: number = Number(process.env.PORT) || 5000;
-// const dataBaseUrl: string = process.env.DATABASE_URL || "default_connection_string";
-
-// console.log(dataBaseUrl);
-
-// async function main() {
-//   try {
-//     await mongoose.connect(dataBaseUrl, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       // Add any other options specific to your MongoDB configuration
-//     });
-//     console.log('db connect successfully');
-//   } catch (err) {
-//     console.log('error occurred in connection', err);
-//   }
-// }
-
-// main().then(() => {
-//   app.listen(port, () => {
-//     console.log(`Server is listening on port ${port}`);
-//   });
-// });
-
-
-
-import mongoose, { ConnectOptions } from 'mongoose';
-import { app } from './app';
-
+import mongoose from 'mongoose'
+import { app } from './app'
 const port: number = Number(process.env.PORT) || 5000;
 const dataBaseUrl: string = process.env.DATABASE_URL || "default_connection_string";
 
-
-
-// Define custom connection options type
-interface CustomConnectOptions extends ConnectOptions {
-  useNewUrlParser: boolean;
-  useUnifiedTopology: boolean;
-  serverApi?: '1';
-}
-
 async function main() {
   try {
-    // Use custom connection options type
-    const options: CustomConnectOptions = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // Add any other options specific to your MongoDB configuration
-    };
-
-    await mongoose.connect(dataBaseUrl, options);
-    console.log('db connect successfully');
+    await mongoose.connect(dataBaseUrl as string)
+    console.log('🛢 database connected succcesfully')
+    //listen
+    
   } catch (err) {
-    console.log('error occurred in connection', err);
+    console.log('failed to coonet in database', err)
   }
 }
-
 main().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+   app.listen(port, () => {
+   console.log(`Server is listening on port ${port}`);
   });
 });
